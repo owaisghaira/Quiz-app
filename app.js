1f54f442f4fff1c253bb19d9d689792fdc468f0e
owaisghaira <owais.ghaira99@gmail.com>
2020-08-16 16:00:14 +0500

    quiz app js  done

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 window.onload =function(){
     show(0)
+    // velidate(0)
 } 
 
 var questions = [{
     ques:"what is your name1",
-    ans: "owais",
+    ans: "owais1",
     option:[
         "owais1",
         "osama1",
@@ -15,7 +16,7 @@ var questions = [{
 },
 {
     ques:"what is your name2",
-    ans: "owais",
+    ans: "osama",
     option:[
         "owais",
         "osama",
@@ -26,7 +27,7 @@ var questions = [{
 },
 {
     ques:"what is your name3",
-    ans: "owais",
+    ans: "sarim",
     option:[
         "owais",
         "osama",
@@ -37,10 +38,15 @@ var questions = [{
 
 ]
 var ques_count = 0;
+var score = 0;
+var count = 0;
 function conut(){
     ques_count++;
-    console.log(ques_count)
+    // console.log(ques_count)
+    velidate(count)
     show(ques_count)
+   
+    // console.log(score)
 
 }
 function show(e){
@@ -53,21 +59,36 @@ function show(e){
                         <li class="option">${questions[e].option[3]}</li>
                     </ul> `;
                     active()
+                    
 }
 
 function active(){
 let option1 = document.querySelectorAll("li.option")
-// console.log(option)
+// console.log(option1)
 for (let i = 0 ; i < option1.length ; i++){
     option1[i].onclick = function(){
         for(let j = 0;j<option1.length;j++){
             if(option1[j].classList.contains("active")){
+                
                 option1[j].classList.remove("active")
             }
 
         }
+        
         option1[i].classList.add("active")
+        // if(option1[i].innerHTML == questions[0].ans){
+        //     score += 10;
+        //     console.log(score)
+        // }
     }
+}
 
 }
+function velidate(a){
+    var active = document.getElementsByClassName("active")
+    if(active[0].innerHTML == questions[a].ans){
+        score += 10;
+        
+    }
+    console.log(score)
 }
